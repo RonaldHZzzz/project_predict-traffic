61adff29ea23d5803d7fc925327a10d8073dc2f0
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-11-24 22:16:23 -0600

    Add GeoDjango models for traffic segments and measurements
    Introduces the initial database schema for the traffic application.
    
    This change defines two core models:
    - `Segmento`: Represents a physical road segment, storing its name and geographical path using a GeoDjango `LineStringField`.
    - `MedicionTrafico`: Captures traffic data for a specific segment at a given time, including average speed and congestion level.

--- backend/trafico/models.py
+++ backend/trafico/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.gis.db import models # Usar siempre gis.db
+
+# trafico_api/models.py
+from django.contrib.gis.db import models
+
+class Segmento(models.Model):
+    # Usaremos el ID del CSV como primary key
+    segmento_id = models.IntegerField(primary_key=True) 
+    nombre = models.CharField(max_length=200)
+    geometria = models.LineStringField(srid=4326) # Aquí guardaremos la línea completa
+    
+    def __str__(self):
+        return self.nombre
+
+class MedicionTrafico(models.Model):
+    segmento = models.ForeignKey(Segmento, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    velocidad_promedio = models.DecimalField(max_digits=5, decimal_places=2)
+    nivel_congestion = models.IntegerField()
