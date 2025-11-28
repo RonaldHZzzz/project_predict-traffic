1f404a7460d6f441061cb315f6f6af9fd14d91e6
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-11-28 01:26:07 -0600

    Add route recommendation endpoint
    Introduces a new API endpoint at `/recommend-route/` to provide clients with the best route or segment based on traffic analysis.

--- backend/traffic_predictor/urls.py
+++ backend/traffic_predictor/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import predict_traffic
+
+from .views import predict_traffic ,get_best_segment
 
 urlpatterns = [
     path("predict-traffic/", predict_traffic, name="predict-traffic"),
+    path("recommend-route/", get_best_segment, name="recommend-best-route"),
+
 ]
