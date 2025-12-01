46842dd8053b65725c12c716fe0739c5b855cad5
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-12-01 01:49:26 -0600

    Adds analytics data endpoint
    Introduces a new API endpoint at `/analytics-data/` to expose traffic analytics data.

--- backend/traffic_predictor/urls.py
+++ backend/traffic_predictor/urls.py
@@ -4,12 +4,14 @@ from .views import (
     predict_traffic,
     get_best_segment,
     recommend_route_v1,
-    recommend_route_v2   
+    recommend_route_v2,
+    get_analytics_data
 )
 
 urlpatterns = [
     path("predict-traffic/", predict_traffic, name="predict-traffic"),
     path("recommend-route/", get_best_segment, name="recommend-best-route"),
     path("recommend-route-v1/", recommend_route_v1, name="recommend-route-v1"),
-    path("recommend-route-v2/", recommend_route_v2, name="recommend-route-v2"),  
+    path("recommend-route-v2/", recommend_route_v2, name="recommend-route-v2"),
+    path("analytics-data/", get_analytics_data, name="analytics-data"),
 ]
