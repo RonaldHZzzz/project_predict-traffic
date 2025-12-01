55ea375749995d13347ae49ea343bf592503e946
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-11-30 22:58:16 -0600

    Add daily metrics endpoint
    Introduces a new URL route, `/metrics/daily/`, to serve daily performance metrics.

--- backend/trafico/urls.py
+++ backend/trafico/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
  
     # Matrix API
     path("matrix/", views.matrix_api, name="matrix_api"),
-
+    # Daily Metrics
+    path("metrics/daily/", views.metricas_diarias, name="metricas_diarias"),
     # Paradas de bus
     path("paradas/", views.paradas_todos_segmentos, name="paradas_todos_segmentos"),
     path(
